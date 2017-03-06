f380165fb7e73a93ebe2f8d4131c4481592ab352
chien.duong <minhchienwikipedia@gmail.com>
2017-03-07 01:35:12 +0700

    Up new

---
+++ index.android.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import FBSDK, {LoginManager} from 'react-native-fbsdk'
+
+export default class LoginFB extends Component {
+  _fbAuth(){
+    LoginManager.logInWithReadPermissions(['public_profile']).then(function(result) {
+      if(result.isCancelled){
+        console.log('Login was cancelled');
+      }else {
+        console.log('Login was a success' + result.grantedPermissions.toString());
+      }
+    },function(error) {
+      console.log('An error occured' + error);
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={()=>this._fbAuth()}>
+          <Text>
+          Login FB
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('LoginFB', () => LoginFB);
